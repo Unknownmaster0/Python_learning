2f291d8e1d5f6365d3dd5e737da4bbb643e26fc3
Unknownmaster0 <sagar192002singh@gmail.com>
2023-09-07 02:14:01 +0530

    python starting

---
+++ escape_sequence_in_python.py
@@ -0,0 +1,14 @@
+print("hello world\nToday is new day of python programming.")
+#we can also print the things in single quote in print function in python
+print('I am good boy\' ')
+print("Yes,\tyou are in\b")
+print("what about\fothers") # \f -> form feed.
+print("what about\vothers") # \v -> vertical tab.
+
+# printing the multiple line of comment in python
+# use ''' -> triple single quote.
+# use """ -> triple double quote.
+
+""" 
+Hii this is sagar singh. I am writing the multi-line comment in python.+
+"""
---
+++ hello_world_in_python.py
@@ -0,0 +1,16 @@
+
+# print("hello world.")
+# print("what is your name.")
+# print("my name is sagar singh.")
+# print("give me",10,"rupee note")
+
+# learning the string in python and veriables.
+name = 'sagar'# we can concatenate the string using the + sign.
+age = 10 # but we can't concatenate the number.
+#we can concatenate the string using the ',' as well as '+' sign.
+# using the plus we have to add the space after the veriable.
+print(name+' a good boy.')
+# the difference in both is -> using the ',' the space is added by default.
+print(name,'is going to take part in Hackathon.')
+print(name+' age is',age)
+print("---your poem here---")
